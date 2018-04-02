86d0a5ada45cd2d2eb3e99b53f69bdb26862405f
khushboo <khushboobhatoia@gmail.com>
2018-04-01 21:15:16 -0400

    Add files via upload

---
+++ Airline/AirlineResevationSystem/src/jDateChooser1.java
@@ -0,0 +1,28 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nikhi
+ */
+class jDateChooser1 {
+
+    static void setDate(Date time) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static Date getDate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setEnabled(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
